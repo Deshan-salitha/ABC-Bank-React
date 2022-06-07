6ce281067942d14fa0a8bfa86730d31b393294a5
Deshan-salitha <71576127+Deshan-salitha@users.noreply.github.com>
2022-06-07 15:43:23 +0530

    Auth

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -13,20 +13,33 @@ const Home = () => {
     var jwt = localStorage.getItem("jwt")
     const navigate = useNavigate();
     const url = 'http://localhost:8080/usebyid/'
-    const signout = (e) => {
+    // const signout = (e) => {
 
-        localStorage.removeItem('jwt')
-        localStorage.removeItem('user')
-        localStorage.removeItem('userid')
-        navigate("/")
-    }
+    //     localStorage.removeItem('jwt')
+    //     localStorage.removeItem('user')
+    //     localStorage.removeItem('userid')
+    //     navigate("/")
+    // }
     const getuser = (e) => {
+        // console.log(jwt);
 
-        axios.post(url + userid, { headers: { "Authorization": `Bearer ${jwt}` } }).then(function (response) {
-
-            console.log(response.data);
-            // localStorage.setItem('auth', JSON.stringify(response))
+        axios.post(
+            url + userid,
+            null,
+            {
+                headers: {
+                    'Authorization': `Bearer ` + jwt,
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+        ).then(function (response) {
+            var user = response.data;
+            // console.log(user.accounts[0].transactins.length);
+            localStorage.setItem('auth', response.data)
+            const users = JSON.parse(localStorage.getItem('auth'))
+            console.log(users)
             // navigator("/home");
+            // user.userEmail
 
         })
     }
@@ -49,19 +62,19 @@ const Home = () => {
                     </div>
                 </header>
                 <div className='welcomming'>
-                    Welcome {user.username}
+                    Welcome {user.userEmail}
                     <button onClick={getuser}>ji</button>
                 </div>
                 <div className='flex p-10 object-center space-x-4 pr-5 flex-row...'>
                     <div className='basis-5/12  bg-white p-8 inline card drop-shadow-xl ...'>
                         <span className='inline-flex space-x-2 items-baseline'>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
-                            <path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
-                        </svg> 
-                        <p className='"text-2xl ..."'>Balance</p>
+                                <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
+                                <path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
+                            </svg>
+                            <p className='"text-2xl ..."'>Balance</p>
                         </span>
-                        
+
                     </div>
                     <div className='basis-5/12 bg-white p-8 card drop-shadow-xl ...'>afalfnl</div>
                 </div>
